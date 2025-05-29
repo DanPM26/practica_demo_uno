90b02f5cab51943ade57d58de4d4c762a8e2ad77
Jhon Doe <jhondoe@gmail.com>
2025-05-28 20:41:00 -0600

    se agregó el ejercicio de operadores

--- index.js
+++ index.js
@@ -27,3 +27,43 @@ if(tiempo >= 3){
 
 console.log(tiempo); 
 console.log(hola); 
+
+//-----------------------------------------
+
+let nota = 20; 
+
+if(nota < 6) {
+    console.log("Reprobamos")
+} else {
+    console.log("Nos vamos de parranda")
+}
+
+// Operador ternario : ( condicion ? verdadero : falso ) 
+let calificacion = nota < 6 ? "reprobamos" : "nos vamos de parranda"; 
+console.log("Operadore ternario", calificacion)
+
+// condicional multiple
+
+if (nota <= 5) {
+    calificacion = "Insuficiente";
+} else if( nota == 6){
+    calificacion = "Suficiente";
+} else if( nota <= 8){
+     calificacion = "Bien"; 
+} else if( nota <= 9) {
+    calificacion = "Notable";
+} else {
+  calificacion = "Sobresaliente"; 
+}
+
+console.log("He obtenido un: ", calificacion); 
+
+// Realizar el mismo ejercicio pero con switch
+
+// for(donde iniciamos; condicion; incremento)
+//[1,2,3,4,5]
+//[0,1,2,3,4]  ->  "<= el alcanzable"; 
+ 
+for(i = 0; i < 5; i++){
+    console.log("valor de i:", i + 1); 
+}
